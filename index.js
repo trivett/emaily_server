937fbe0588bcc21b2a35bff00c63caf5343c2796
Vincent Trivett <trivett@gmail.com>
2017-12-10 13:02:36 -0500

    starter setup for mongo, passport, cookie-session

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys.js");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile:", profile);
-      console.log("done", done);
-    }
-  )
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
 );
 
-/** AUTH */
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+app.use(passport.initialize());
 
-app.get("/auth/google/callback", passport.authenticate("google"));
+app.use(passport.session());
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 
