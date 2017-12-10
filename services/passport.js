795c817a58ba4632b6c7f37a31f58d96a48e80bb
Vincent Trivett <trivett@gmail.com>
2017-12-10 17:02:45 -0500

    oooo i like async await

--- services/passport.js
+++ services/passport.js
@@ -23,16 +23,15 @@ passport.use(
       proxy: true,
       callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save().then(user => {
-            done(null, user);
-          });
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
