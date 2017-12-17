d5e77e3a3e4d8f20f1455271c617cdc2dd8e2c87
Vincent Trivett <trivett@gmail.com>
2017-12-17 08:40:22 -0500

    stripe checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
+import Payments from "./Payments";
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
@@ -14,11 +14,17 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="3">
             <a href="/api/logout">Log out</a>
           </li>
-        );
+        ];
     }
   }
 
